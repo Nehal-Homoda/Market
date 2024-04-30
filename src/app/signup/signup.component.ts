6b1fdf3e352cd72672dc0c3a4e69fb08452e48ab
nehal <nehalhomoda00@gmail.com>
2024-04-30 05:26:45 +0300

    access to forms element

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,24 +7,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-  userReg:FormGroup;
-  constructor(private fb:FormBuilder){
-    this.userReg=this.fb.group({
-      firstName:['',Validators.required],
-      LastName:['',Validators.required],
-      Email:['',Validators.required],
-      Password:['',Validators.required],
-      ConfirmedPassword:['',Validators.required]
-
-
-    })
-
-
-
+  userReg: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.userReg = this.fb.group({
+      firstName: ['', Validators.required],
+      LastName: ['', Validators.required],
+      Email: ['', Validators.required],
+      Password: ['', Validators.required],
+      ConfirmedPassword: ['', Validators.required],
+    });
+  }
+  get fisrtName() {
+    return this.userReg.get('firstName');
+  }
+  get lastName() {
+    return this.userReg.get('LastName');
+  }
+  get email(){
+    return this.userReg.get('Email');
+  }
+  get password(){
+    return this.userReg.get('Password');
 
   }
-
+  get confirmedPassword(){
+    return this.userReg.get('ConfirmedPassword');
+  }
 }
