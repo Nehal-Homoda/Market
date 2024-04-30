ce7a448020c2f39a8cf90d21617e904c5874918e
nehal <nehalhomoda00@gmail.com>
2024-04-30 04:21:28 +0300

    add sign up reactive form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  userReg:FormGroup;
+  constructor(private fb:FormBuilder){
+    this.userReg=this.fb.group({
+      firstName:['',Validators.required],
+      LastName:['',Validators.required],
+      Email:['',Validators.required],
+      Password:['',Validators.required],
+      ConfirmedPassword:['',Validators.required]
+
+
+    })
+
+
+
+
+  }
 
 }
